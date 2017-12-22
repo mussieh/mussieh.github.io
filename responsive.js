ca84d6fa77f9df8f2f950f4316820cb95acf3c96
Moses <numbav@gmail.com>
2017-12-21 21:15:29 -0600

    Content change

--- responsive.js
+++ responsive.js
@@ -1,5 +1,7 @@
 "use strict";
+window.addEventListener("load", main)
 
+function main() {
     // Used to toggle the menu on small screens when clicking on the menu button
     function myFunction() {
         var navDemo = document.getElementById("navDemo");
@@ -9,5 +11,4 @@
             navDemo.className = navDemo.className.replace(" w3-show", "");
         }
     }
-
-window.onload = myFunction;
+}
