5f4567a5a3e71c3da03aa271a4208d8a01115288
Moses <numbav@gmail.com>
2018-01-01 02:54:08 -0600

    js change

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,138 @@
-window.onload=main;
-function main(){var a,b=document.getElementsByClassName("iplay"),d=window.innerHeight,e,n,k,l,m,p,c,g=document.getElementById("toggleButton"),h=document.getElementById("largeToggleButton"),q=document.getElementById("myphoto"),r=document.getElementById("theme-style"),t=document.getElementById("favicon"),f;document.getElementById("navSelect").addEventListener("click",toggleNavMenu);null!=g&&g.addEventListener("click",function(){f&&window.clearTimeout(f);f=window.setTimeout(function(){switchTheme(g,h,
-q,r,t)},500)});null!=h&&h.addEventListener("click",function(){f&&window.clearTimeout(f);f=window.setTimeout(function(){switchTheme(g,h,q,r,t)},500)});-1!=document.location.href.indexOf("projects")&&(window.addEventListener("scroll",function(){a&&window.clearTimeout(a);a=window.setTimeout(function(){for(c=0;c<b.length;c++)k=b[c].getBoundingClientRect(),n=k.height/2,e=k.y+n+50,l=b[c].src.indexOf("pause"),p=b[c].src.lastIndexOf("_"),m=b[c].src.substring(0,p),0<e&&e<d+50?-1!=l&&(b[c].src=m+"_.gif"):-1==
-l&&(b[c].src=m+"_pause.gif")},200)}),window.onresize=function(){d=window.innerHeight})}
-function switchTheme(a,b,d,e){-1!=a.className.indexOf("fa-toggle-off")?(null!=d&&(d.src="img/bp.png"),localStorage.setItem("theme","dark"),favicon.href="img/favbr.png",e.href="css/dark.css",a.className="fa fa-toggle-on w3-hover-opacity w3-padding-small",b.className="fa fa-toggle-on w3-xlarge w3-hover-opacity w3-padding-small"):(null!=d&&(d.src="img/lp.png"),localStorage.clear(),favicon.href="img/favbl.png",e.href="css/light.css",a.className="fa fa-toggle-off w3-hover-opacity w3-padding-small",b.className=
-"fa fa-toggle-off w3-xlarge w3-hover-opacity w3-padding-small")}function toggleNavMenu(){var a=document.getElementById("navDemo");-1==a.className.indexOf("w3-show")?a.className+=" w3-show":a.className=a.className.replace(" w3-show","")};
+"use strict";
+
+window.onload = main;
+
+function main() {
+
+    var timer;
+    var projectViews = document.getElementsByClassName("iplay");
+    var windowHeight = window.innerHeight;
+    var vanishPosition;
+    var midwayPoint;
+    var boundingRectangle;
+    var imagePauseStatus;
+    var imageSrc;
+    var underscorePosition; // "_" used to divide pause and play gifs
+    var counter;
+    var toggleButton = document.getElementById("toggleButton");
+    var largeToggleButton = document.getElementById("largeToggleButton");
+    var myPhotoObj = document.getElementById("myphoto");
+    var themeStyle = document.getElementById("theme-style");
+    var favicon = document.getElementById("favicon");
+    var toggleId;
+
+    document.getElementById("navSelect").addEventListener("click", toggleNavMenu);
+
+    if (toggleButton != null) {
+
+        toggleButton.addEventListener("click", function() {
+            if (toggleId) {
+                window.clearTimeout(toggleId);
+            }
+
+            toggleId = window.setTimeout(function() {
+
+                switchTheme(toggleButton, largeToggleButton, myPhotoObj, themeStyle, favicon);
+
+            }, 500); // Set click event listening delay
+        });
+    }
+
+    if (largeToggleButton != null) {
+
+        // Toggle button for smaller screens
+        largeToggleButton.addEventListener("click", function() {
+            if (toggleId) {
+                window.clearTimeout(toggleId);
+            }
+
+            toggleId = window.setTimeout(function() {
+
+                switchTheme(toggleButton, largeToggleButton, myPhotoObj, themeStyle, favicon);
+
+            }, 500); // Set click event listening delay
+        });
+
+    }
+
+    alert(document.location.href);
+    if (document.location.href.indexOf("projects") != -1) {
+
+        window.addEventListener("scroll", function() {
+            if (timer) {
+                window.clearTimeout(timer);
+            }
+
+            timer = window.setTimeout(function() {
+                for (counter = 0; counter < projectViews.length; counter++) {
+                    boundingRectangle = projectViews[counter].getBoundingClientRect();
+                    midwayPoint = boundingRectangle.height / 2;
+                    vanishPosition = boundingRectangle.y + midwayPoint + 50 // 50 for navbar height;
+                    imagePauseStatus = projectViews[counter].src.indexOf("pause");
+                    underscorePosition = projectViews[counter].src.lastIndexOf("_");
+                    imageSrc = projectViews[counter].src.substring(0, underscorePosition);
+
+                    if (vanishPosition > 0 && vanishPosition < windowHeight + 50) { // image is in view
+                        if (imagePauseStatus == -1) {} // if playing, do nothing
+                        else {
+                            projectViews[counter].src = imageSrc + "_.gif";
+                        }
+                    }
+                    // image hidden
+                    else {
+                        if (imagePauseStatus == -1) {  // if playing, pause
+                            projectViews[counter].src = imageSrc + "_pause.gif";
+                        }
+                    }
+                }
+            }, 200); // Set scrolling event listening delay to save CPU resources
+        });
+
+        window.onresize = function() {
+            windowHeight = window.innerHeight;
+        };
+    }
+}
+
+
+function switchTheme(toggleButton, largeToggleButton, myPhotoObj, themeStyle) {
+
+    // enabling dark theme
+    if (toggleButton.className.indexOf("fa-toggle-off") != -1) {
+        if (myPhotoObj != null) {
+            myPhotoObj.src = "img/bp.png";
+        }
+        localStorage.setItem("theme", "dark");
+        favicon.href = "img/favbr.png";
+        themeStyle.href = "css/dark.css";
+
+        toggleButton.className = "fa fa-toggle-on w3-hover-opacity w3-padding-small";
+        largeToggleButton.className = "fa fa-toggle-on w3-xlarge w3-hover-opacity w3-padding-small";
+
+    }
+
+    // going back to blue theme
+    else {
+
+        if (myPhotoObj != null) {
+            myPhotoObj.src = "img/lp.png";
+        }
+        localStorage.clear();
+        favicon.href = "img/favbl.png";
+        themeStyle.href = "css/light.css";
+
+        toggleButton.className = "fa fa-toggle-off w3-hover-opacity w3-padding-small";
+        largeToggleButton.className = "fa fa-toggle-off w3-xlarge w3-hover-opacity w3-padding-small";;
+    }
+}
+
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleNavMenu() {
+    var navDemo = document.getElementById("navDemo");
+    if (navDemo.className.indexOf("w3-show") == -1) {
+        navDemo.className += " w3-show";
+    } else {
+        navDemo.className = navDemo.className.replace(" w3-show", "");
+    }
+}
